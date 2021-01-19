b74a5a5becf3129d44667a16717ea2c98d319a46
MustafaAmer-1 <53836898+MustafaAmer-1@users.noreply.github.com>
2021-01-19 22:54:46 +0200

    Add requirements script
    Add python requirements script

---
+++ requirements.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+
+
+def install(package):
+    os.system("pip install " + str(package))
+    reqs = subprocess.check_output([sys.executable, "-m", "pip", "show", str(package)])
+
+    print(str(reqs) + "\n")
+    print("Installed " + package.upper() + "\n")
+
+install('requests')
+install('beautifulsoup4')
+install('termcolor')
+install('colorama')
+install('selenium')
+install('webdriver_manager')
